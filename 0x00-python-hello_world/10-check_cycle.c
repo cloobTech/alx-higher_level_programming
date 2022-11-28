075aa1fcb60402be5ca78fd54d9bde8f280030b1
cloobTech <belkid98@gmail.com>
2022-11-28 13:54:27 -0800

    task

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,14 @@
+#include "list.h"
+
+/**
+ * check_cycle - check for circle in a linked list
+ *
+ * @list: Pointer to the head node
+ *
+ * Return: 0 for success else 1
+ */
+
+int check_cycle(listint_t *list)
+{
+	return (0);
+}
