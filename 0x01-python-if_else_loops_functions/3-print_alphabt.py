2de9cb469f5dc2abcdd5469a7a6f58c758e9ad6a
cloobTech <belkid98@gmail.com>
2022-11-29 05:14:44 -0800

    task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@
 for i in range(97, 123):
     if chr(i) == 'q' or chr(i) == 'e':
         continue
-    print(chr(i), end="")
+    print("{}".format(chr(i)), end="")
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for i in range(0, 99):
-    print(f"{i:d} = 0x{i:x}")
+    print("{num:d} = 0x{hex:x}".format(num=i, hex=i))
