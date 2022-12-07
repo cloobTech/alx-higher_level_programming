d229d4023847592c44a1de5af656bc7f90b1355c
cloobTech <belkid98@gmail.com>
2022-12-07 11:28:42 -0800

    unique

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    new_list = []
-    dup_list = []
-    my_list = (list(set_1) + list(set_2))
-    for item in my_list:
-        if item == "":
-            continue
-        if item not in new_list:
-            new_list.append(item)
-        else:
-            dup_list.append(item)
-    return set(new_list)
+    my_set = set_1.union(set_2)
+    return my_set
