8587c27f8b92d7381fc478c90d9c4e5fa9ece4d5
cloobTech <belkid98@gmail.com>
2022-12-07 05:19:30 -0800

    yask

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
+    if  my_list == None:
+        return
     idx = len(my_list)
     for item in range(0, idx):
         idx -= 1
