a92ba1de9eac4a2ad2f0d02ab0f8e23a5eb5b82b
cloobTech <belkid98@gmail.com>
2022-12-07 10:13:20 -0800

    task

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_list = []
+    for i in range(0, len(my_list)):
+        new_list.append(my_list[i])
+        if i + 1 == search:
+            new_list[i] = replace
+    return new_list
