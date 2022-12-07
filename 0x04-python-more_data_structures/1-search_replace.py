dbdac27029f060d9539682096f6bdd132b41ea6a
cloobTech <belkid98@gmail.com>
2022-12-07 10:16:57 -0800

    search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,6 +3,8 @@
 def search_replace(my_list, search, replace):
     new_list = []
     for i in range(0, len(my_list)):
+        if search > len(my_list):
+            return my_list
         new_list.append(my_list[i])
         if i + 1 == search:
             new_list[i] = replace
