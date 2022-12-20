e0a30827fa843a51d721da5c16c74cd4519b80ff
cloobTech <belkid98@gmail.com>
2022-12-20 05:58:35 -0800

    empty

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 
-    """A simple script that defines an empty
-    class Square that defines a square
-    """
+"""A simple script that defines an empty class Square that defines a square"""
 
 
 class Square:
