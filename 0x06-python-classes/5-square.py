fbf989b9961c6fb87198e518e0432fa8cf6e87ef
cloobTech <belkid98@gmail.com>
2022-12-20 19:08:52 -0800

    hashes

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+""" Write a class Square that defines a square by: (based on 1-square.py)."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+            size (integer): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Return the area of the square """
+        return (self.__size * self.__size)
+    @property
+    def size(self):
+        """ Getter method to return size """
+        return (self.__size)
+    @size.setter
+    def size(self, value):
+        """ Setter method to set size """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    def my_print(self):
+        """ print # """
+        if self.__size == 0:
+            print("{}".format('\n'), end="")
+        for i in range(0, self.__size):
+            for j in range(0, self.__size):
+                print("{}".format('#'), end="")
+            print("{}".format('\n'), end="")
