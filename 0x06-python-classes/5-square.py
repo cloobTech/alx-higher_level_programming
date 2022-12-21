ab83f86b84cf616b1d163e2cc7ebbce568571bb5
cloobTech <belkid98@gmail.com>
2022-12-20 19:11:08 -0800

    hashes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -20,10 +20,12 @@ class Square:
     def area(self):
         """ Return the area of the square """
         return (self.__size * self.__size)
+
     @property
     def size(self):
         """ Getter method to return size """
         return (self.__size)
+
     @size.setter
     def size(self, value):
         """ Setter method to set size """
@@ -32,6 +34,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def my_print(self):
         """ print # """
         if self.__size == 0:
