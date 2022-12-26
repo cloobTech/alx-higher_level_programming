74900aef0239797c76fc2cd9e6026f23eb34645f
cloobTech <belkid98@gmail.com>
2022-12-25 16:32:07 -0800

    7

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+""" Write a class Square that defines a square by: (based on 1-square.py)."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new Square.
+        Args:
+            size (integer): The size of the new square.
+            position (integer, integer): Sets the position of a square.
+        """
+        self.size = size
+        self.position = position
+
+    def area(self):
+        """ Return the area of the square """
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """ Getter method to return size """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """ Setter method to set size """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """ Getter to retrieve position """
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        """ Set the position of the square """
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+
+    def __str__(self):
+        """ 
+
+    def my_print(self):
+        """ print # """
+        if self.__size == 0:
+            print("{}".format('\n'), end="")
+            return
+        for pos in range(0, self.__position[1]):
+            print("")
+        for i in range(0, self.__size):
+            for pos1 in range(0, self.__position[0]):
+                print("{}".format(" "), end="")
+            for j in range(0, self.__size):
+                print("{}".format('#'), end="")
+            print("{}".format('\n'), end="")
--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -17,6 +17,7 @@ class MagicClass:
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
+        return None
 
     def area(self):
         """Return the area of the MagicClass."""
