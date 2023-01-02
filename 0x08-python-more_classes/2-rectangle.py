bcd7f2bda74781415f24c74cb74ec2e604a49a69
cloobTech <belkid98@gmail.com>
2023-01-02 07:06:34 -0800

    permeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+""" A script with class that represents a Rectantangle"""
+
+
+class Rectangle:
+    """  A class Rectangle that defines a rectangle: """
+    def __init__(self, width=0, height=0):
+        """ Initialize an instance of the class Rectangle.
+        Args:
+            width(int): width of the rectangle
+            height(int): Height of the rectangle
+        Raises:
+            TypeError: width must be an integer
+            TypeError: height must be an integer
+            ValueError: width must be >= 0
+            ValueError: height must be >= 0
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """getter method"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """ setter method """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter method"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """ setter method """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ returns the area of the rectangle"""
+        return (self.width * self.height)
+
+    def perimeter(self):
+        """ returns the perimeter of the rectangle"""
+        if self.width == 0 or self.height == 0:
+            p = 0
+        else:
+            p = 2 * (self.width + self.height)
+
+        return (p)
