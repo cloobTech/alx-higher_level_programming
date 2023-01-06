9ec65c00e6351d8795c690014cb7025e00b79064
cloobTech <belkid98@gmail.com>
2023-01-06 02:02:40 -0800

    TDD

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -2,8 +2,7 @@
 
 def print_square(size):
     """a function that prints a square with the character #."""
-    if (not isinstance(size, int) or (isinstance(size, float) and
-            size < 0)):
+    if (not isinstance(size, int) or (isinstance(size, float) and size < 0)):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
