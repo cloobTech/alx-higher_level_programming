fb7597d9f702ae43030f4dbd4dc2833c60697754
cloobTech <belkid98@gmail.com>
2023-01-10 07:08:39 -0800

    read

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" A script containing a function that reads a file"""
+
+
+def read_file(filename=""):
+    """ a function that reads a text file (UTF8) and prints it to stdout:
+    Args:
+        filename(str): file path
+    """
+    with open(filename) as read_file:
+        content = read_file.read()
+        print(content)
