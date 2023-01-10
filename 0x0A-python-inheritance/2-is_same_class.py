a40cf8faac95ef39637a9d028d54c9dcaf1066da
cloobTech <belkid98@gmail.com>
2023-01-10 02:13:48 -0800

    ype

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -9,7 +9,4 @@ def is_same_class(obj, a_class):
         obj(obj): first parameter
         a_class (class or obj): Second Parameter
     """
-    if isinstance(obj, a_class):
-        return (True)
-    else:
-        return (False)
+   return type(obj) == a_class
