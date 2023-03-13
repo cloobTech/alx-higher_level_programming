fb504c404c17f46ca037ecad167e2167e5f65d50
cloobTech <belkid98@gmail.com>
2023-03-13 06:41:34 -0700

    argv

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+import { argv } from 'node:process';
+let result = '';
+
+// print process.argv
+argv.forEach((val, index) => {
+if (index === 1) {
+  result = 'No argument';
+} else {
+  result = 'Argument found';
+}
+});
+
+console.log(result);
