1e2660e34cd57d66956acb7e6d5a48e1576546af
cloobTech <belkid98@gmail.com>
2023-03-14 08:17:52 -0700

    square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,13 +7,12 @@ const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
-    this.size = size;
   }
 
   charPrint (c = 'X') {
     let i = 0;
-    while (i < this.size) {
-      console.log(c.repeat(this.size));
+    while (i < this.width) {
+      console.log(c.repeat(this.width));
       i++;
     }
   }
