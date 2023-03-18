b2bbcab1c9b8ceb7733f463850e60cf57b19a11f
cloobTech <belkid98@gmail.com>
2023-03-18 03:11:22 -0700

    2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,13 @@ if __name__ == '__main__':
             charset="utf8")
 
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4]))
+    cur.execute(
+            """
+            SELECT * FROM states WHERE
+            name = BINARY'{}'
+            ORDER BY id ASC
+            """.format(sys.argv[4])
+            )
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
