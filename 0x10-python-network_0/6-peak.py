b4b82d7a5cb19676df9690a862a5445ec3615424
cloobTech <belkid98@gmail.com>
2023-03-30 19:25:34 -0700

    interview

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,8 +9,10 @@ def find_peak(list_of_integers):
     low, high = 0, len(list_of_integers) - 1
     while low <= high:
         mid = (low + high) // 2
-        if (mid == 0 or list_of_integers[mid - 1] <= list_of_integers[mid]) and \
-                (mid == len(list_of_integers) - 1 or list_of_integers[mid] >= list_of_integers[mid + 1]):
+        if (mid == 0 or list_of_integers[mid - 1]
+                <= list_of_integers[mid]) and \
+                (mid == len(list_of_integers) - 1 or
+                 list_of_integers[mid] >= list_of_integers[mid + 1]):
             return list_of_integers[mid]
         elif mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
             high = mid - 1
