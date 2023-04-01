dc267aeee3056667970ea4049a49f1da1380a10a
cloobTech <belkid98@gmail.com>
2023-03-31 17:27:16 -0700

    handle

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -13,6 +13,6 @@ if __name__ == '__main__':
     url_obj = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(url_obj) as res:
-            print(res.read().decode('utf-8'))
+            print('Index')
     except urllib.error.HTTPError as e:
         print(f'Error code: {e.code()}')
