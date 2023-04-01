e1c2d11d12a22a23b2be9e8433a015368aed11eb
cloobTech <belkid98@gmail.com>
2023-04-01 10:57:58 -0700

    requests

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+if len(sys.argv) == 2:
+    q = sys.argv[1]
+else:
+    q = ""
+payload = {'q': q}
+url = 'http://0.0.0.0:5000/search_user'
+response = requests.post(url, data=payload)
+try:
+    res_json = response.json()
+    if res_json:
+        print(f'[{res_json.get("id")}] {res_json.get("name")}')
+    else:
+        print("No result")
+except ValueError:
+    print("Not a valid JSON")
