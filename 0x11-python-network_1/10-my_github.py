8471babaee60cadd30302e59659b1bf06393aa45
cloobTech <belkid98@gmail.com>
2023-04-03 13:24:25 -0700

    auth

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """
-a Python script that takes your GitHub credentials (username and password)
-and uses the GitHub API to display your id"""
-import sys
-import requests
+Write a Python script that takes your GitHub
+credentials (username and password)
+and uses the GitHub API to display your id
+"""
 
+if __name__ == "__main__":
+    import requests
+    from sys import argv
 
-user_name = sys.argv[1]
-password = sys.argv[2]
-auth = requests.auth.HTTPBasicAuth(user_name, password)
-url = f'https://api.github.com/user'
+    url = f"https://api.github.com/users/{argv[1]}"
+    header = {}
+    header['Accept'] = 'application/vnd.github+json'
+    header['X-GitHub-Api_version'] = "2022-11-28"
+    header['Authorization'] = f"Bearer {argv[2]}"
 
-response = requests.get(url, auth=auth)
-try:
+    response = requests.get(url, headers=header)
     data = response.json()
-    user_id = data.get('id')
-    print(user_id)
-except Exception as err:
-    print('None')
+    print(data.get('id'))
